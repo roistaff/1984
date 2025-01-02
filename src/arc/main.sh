2c065e7847eabb831430e0a854bddbd72fb24bee
drstrange1984 <hellocreater1983@gmail.com>
2025-01-02 16:50:05 +0900

    test98

--- src/arc/main.sh
+++ src/arc/main.sh
@@ -2,4 +2,4 @@ for file in `\find . -maxdepth 1 -name '*.md'`; do
 	echo $file
 	pandoc $file -s -o $HOME/public/$(basename $file .md).html
 done
-## cat ~/public/README.html
+## cat ~/public/README.######html
